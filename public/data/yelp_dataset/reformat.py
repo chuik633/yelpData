de8ccce316f6b97943010863a9987e6285c72842
Katherine Chui <chuik633@newschool.edu>
2025-06-10 12:50:31 -0400

    lf

--- public/data/yelp_dataset/reformat.py
+++ public/data/yelp_dataset/reformat.py
@@ -1,10 +1,10 @@
 import json
 
-with open('data/yelp_dataset/business.json', 'r') as input_file:
+with open('./public/data/yelp_dataset/business.json', 'r') as input_file:
     lines = input_file.readlines()
-    data = [json.loads(line) for line in lines]
+    data = [json.loads(line) for line in lines[:80000]]
 
-with open('data/yelp_dataset/business_reformatted.json', 'w') as output_file:
+with open('./public/data/yelp_dataset/business_reformatted.json', 'w') as output_file:
     json.dump(data, output_file, indent=4)
 
 
