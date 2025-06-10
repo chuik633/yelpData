31e913ff09782a8b739cbd09f1e4b7f771f16e28
Katherine Chui <chuik633@newschool.edu>
2025-06-10 11:25:34 -0400

    large file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "./",
   build: {
     outDir: "docs", // Ensure the output directory is set to 'docs'
     assetsDir: "assets", // Specify the assets directory
